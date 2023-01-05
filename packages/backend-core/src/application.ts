fb46a4c8188b01d176dc071bc1f506fda2e58acb
Ranjeet Kumar <r.kumar@tubics.com>
2023-01-05 20:04:51 +0100

    feat: setting up simple error handler and basic middlewares

---
+++ packages/backend-core/src/application.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+console.log(bodyParser);
+export class Application  {
+    private _expressServer: express.Application;
+
+    run(port: number) {
+        this._expressServer.listen(port, () => {
+            console.log(`Server is listening on prt ${port}`);
+        });
+    }
+    private _setupMiddleware(app: express.Application) {
+        app.use(bodyParser.json('application/json'));
+        app.use(bodyParser.urlencoded({extended: true}));
+    }
+
+    private _setupErrorMiddleware(app: express.Application) {
+        //handling all errors: known and unknown
+        //error middleware always takes four arguments: https://expressjs.com/en/guide/error-handling.html
+        app.use((error: any, request: express.Request, response: express.Response, next: express.NextFunction) => {
+            const {data }= error;
+            const message = error.message || data?.message;
+            const code = error._code || data?.code;
+            console.error(`Error ocurred: ${message}`, {
+                meta: {
+                    message, 
+                    code,
+                    stack: error.stack
+                }
+            });
+            return response.status(500).send(message);
+        });
+    }
+ 
+}
--- packages/backend-core/src/run-service.ts
+++ packages/backend-core/src/run-service.ts
@@ -10,3 +10,4 @@ export default async function start(port) {
         process.exit(0);
     }
 }
+
